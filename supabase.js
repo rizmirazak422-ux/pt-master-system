33a2164f1e78a1ef60776ec5a4104710b7ee4103
rizmirazak422-ux <rizmirazak422@gmail.com>
2026-06-11 12:33:49 +0800

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,6 @@
+lib/import { createClient } from "@supabase/supabase-js"
+
+const supabaseUrl = "YOUR_SUPABASE_URL"
+const supabaseKey = "YOUR_SUPABASE_KEY"
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
